18afc6b4cec01142ca756787034393770c15ad18
ani11012003 <108975127+ani11012003@users.noreply.github.com>
2024-04-21 21:49:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function genQR() {
-    var gapi = "https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
+    var gapi = "https://chart.googleapis.com/chart?cht=qr&chs=";
     var myimg = document.getElementById("img");
     var mytext = document.getElementById("qrtext").value;
     var mysize = document.getElementById("size").value;
